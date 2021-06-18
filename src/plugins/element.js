e713cf96d42b791935fdc955e3530b88dcaad5b4
aonly <atiffiany@outlook.com>
2021-06-18 18:22:30 +0800

    登陆功能实现

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,10 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import { Button,Form, FormItem, Input, Message } from 'element-ui'
 
+// Vue.use()将引入的组件设为全局可用
 Vue.use(Button)
+Vue.use(Form);
+Vue.use(FormItem);
+Vue.use(Input);
+// Message组件需要进行全局挂载
+Vue.prototype.$message = Message
